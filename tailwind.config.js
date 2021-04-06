b47d32a841440e009c5e4443e7f4db82bb48084f
Moloy Pal <77716576+moloypal2021@users.noreply.github.com>
2021-04-06 19:55:41 +0600

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,15 @@ module.exports = {
     extend: {
       colors: {
         "light-nav": "#F3F5F6",
-        success: "#22C062",
+        success: {
+          DEFAULT: "#22C062",
+          alt: "#31AF68",
+        },
         danger: "#FF0000",
+        paragraph: "#6E8095",
+      },
+      boxShadow: {
+        d: "0px 10px 30px rgba(0, 0, 0, 0.1)",
       },
     },
   },
